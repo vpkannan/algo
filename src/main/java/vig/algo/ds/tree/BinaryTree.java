b1f2acd87fed0e28260554a8840fb89aec0fa5a4
Vignesh Kannan <vignesh.kannan@yugacle.com>
2017-02-10 19:46:25 -0600

    implement basic binary tree pojo

--- src/main/java/vig/algo/ds/tree/BinaryTree.java
+++ src/main/java/vig/algo/ds/tree/BinaryTree.java
@@ -4,6 +4,50 @@ package vig.algo.ds.tree;
  * @author Vignesh
  *
  */
-public class BinaryTree {
+public class BinaryTree<T> {
+
+	T val;
+	private BinaryTree<T> left;
+	private BinaryTree<T> right;
+
+	public BinaryTree() {
+		super();
+	}
+
+	public BinaryTree(T val) {
+		super();
+		this.val = val;
+	}
+
+	public BinaryTree(T val, BinaryTree<T> left, BinaryTree<T> right) {
+		super();
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+
+	public T getVal() {
+		return val;
+	}
+
+	public void setVal(T val) {
+		this.val = val;
+	}
+
+	public BinaryTree<T> getLeft() {
+		return left;
+	}
+
+	public void setLeft(BinaryTree<T> left) {
+		this.left = left;
+	}
+
+	public BinaryTree<T> getRight() {
+		return right;
+	}
+
+	public void setRight(BinaryTree<T> right) {
+		this.right = right;
+	}
 
 }
