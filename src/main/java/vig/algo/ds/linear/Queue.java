44ab01a389cc4c35e98e0fc17e8df3fca3c25269
Vignesh Kannan <vignesh.p.kannan@gmail.com>
2017-02-13 20:38:44 -0600

    init queue

--- src/main/java/vig/algo/ds/linear/Queue.java
+++ src/main/java/vig/algo/ds/linear/Queue.java
@@ -3,17 +3,27 @@
  */
 package vig.algo.ds.linear;
 
+import java.lang.reflect.Array;
+
 /**
  * @author Vignesh
  *
  */
 public class Queue<T> {
-	
-	T[] queue;
-	
+
+	private T[] queue;
+	private int front;
+	private int back;
+
 	public Queue() {
 		super();
 	}
-	
-	
+
+	public Queue(Class<T> type, int size) {
+		super();
+		this.queue = (T[]) Array.newInstance(type, size);
+		this.front = 0;
+		this.back = 0;
+	}
+
 }
